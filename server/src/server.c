5ddc6b38a8b8da96defdba28cd092f0cb2295e2a
Tiago Dall'Oca <tdalloca@gmail.com>
2023-04-23 22:58:53 -0300

    add server commands very good ~ mwah ~

--- server/src/server.c
+++ server/src/server.c
@@ -7,7 +7,6 @@
 #include <pthread.h>
 #include <signal.h>
 #include <dirent.h>
-#include <string.h>
 
 #define PORT 5000                       // Número da porta para o servidor
 #define MAX_CLIENTS 10                  // Número máximo de clientes concorrentes
@@ -29,13 +28,14 @@ void manipulador_sigint(int signum) {
     running = 0;
 }
 
-void imprimir_arquivos_na_pasta(const char* caminho) {
+const char *listar_todos_usuarios(const char *caminho) {
     DIR *dir;
     struct dirent *ent;
+    json_object *users_array = json_object_new_array(); // Create JSON array object
 
     if ((dir = opendir(caminho)) == NULL) {
         perror("Não foi possível abrir o diretório");
-        return;
+        return NULL;
     }
 
     while ((ent = readdir(dir)) != NULL) {
@@ -47,28 +47,191 @@ void imprimir_arquivos_na_pasta(const char* caminho) {
         char caminho_completo[100];
         sprintf(caminho_completo, "%s/%s", caminho, ent->d_name);
 
-        FILE* fp = fopen(caminho_completo, "r");
+        FILE *fp = fopen(caminho_completo, "r");
         if (fp == NULL) {
             printf("Não foi possível abrir o arquivo %s\n", caminho_completo);
             continue;
         }
 
-        printf("Arquivo: %s\n", caminho_completo);
+        // Read user information from file and create JSON object
+        json_object *user_obj = json_object_new_object();
+        json_object_object_add(user_obj, "email", json_object_new_string(ent->d_name));
 
-        char buffer[4096];
-        while (fgets(buffer, sizeof(buffer), fp)) {
-            printf("%s", buffer);
-        }
-
-        printf("\n");
+        char buffer[10000];
+        fgets(buffer, sizeof(buffer), fp);
+        json_object *json_obj = json_tokener_parse(buffer);
+        json_object_array_add(users_array, json_obj); // Add user object to JSON array
         fclose(fp);
     }
 
     closedir(dir);
+
+    // Convert JSON array to string and return
+    const char *json_str = json_object_to_json_string(users_array);
+
+    size_t len = strlen(json_str);
+    char *resp = (char *)malloc(len + 1);
+    strncpy(resp, json_str, len);
+    resp[len] = '\0';
+
+    json_object_put(users_array); // Release JSON array memory
+
+    return resp;
+}
+
+// Function to delete user file based on email
+void deletar_usuario(const char *email, const char *caminho) {
+    char caminho_completo[100];
+    sprintf(caminho_completo, "%s/%s.json", caminho, email);
+
+    if (remove(caminho_completo) != 0) {
+        printf("Não foi possível deletar o arquivo %s\n", caminho_completo);
+    } else {
+        printf("Arquivo %s deletado com sucesso.\n", caminho_completo);
+    }
+}
+
+int busca_substring(const char *texto, const char *subtexto) {
+    unsigned int tamanho_texto = strlen(texto);
+    unsigned int tamanho_subtexto = strlen(subtexto);
+    if (tamanho_texto < tamanho_subtexto) {
+        return 0; // Retorna falso se o texto é menor que o subtexto
+    }
+    for (size_t i = 0; i <= tamanho_texto - tamanho_subtexto; i++) {
+        if (strncasecmp(texto + i, subtexto, tamanho_subtexto) == 0) {
+            return 1; // Retorna verdadeiro se encontrar uma correspondência de substring
+        }
+    }
+    return 0; // Retorna falso se nenhuma correspondência de substring for encontrada
+}
+
+const char *query_usuarios(const json_object *payload, const char *caminho) {
+    json_object *user_array = json_object_new_array(); // Create a new JSON array
+
+    DIR *dir;
+    struct dirent *ent;
+
+    if ((dir = opendir(caminho)) == NULL) {
+        perror("Não foi possível abrir o diretório");
+        return NULL;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+        // Ignorar . e ..
+        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+            continue;
+        }
+
+        char caminho_completo[100];
+        sprintf(caminho_completo, "%s/%s", caminho, ent->d_name);
+
+        FILE *fp = fopen(caminho_completo, "r");
+        if (fp == NULL) {
+            printf("Não foi possível abrir o arquivo %s\n", caminho_completo);
+            continue;
+        }
+
+        char buffer[4096];
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            json_object *user = json_tokener_parse(buffer); // Parse JSON from file
+            if (user != NULL) {
+                int match = 1; // Flag to indicate if user matches fields in payload
+                json_object_object_foreach(payload, key, val) {
+                    json_object *user_value = json_object_object_get(user, key);
+                    if (user_value != NULL) {
+                        const char *user_value_str = json_object_get_string(user_value);
+                        const char *payload_value_str = json_object_get_string(val);
+                        if (strcasecmp(key, "habilidades") == 0) {
+                            if (busca_substring(user_value_str, payload_value_str) == NULL) {
+                                match = 0; // Set match flag to false if habilidades do not partially match
+                                break; // Break loop if a mismatch is found
+                            }
+                        }
+                        else if (strcmp(user_value_str, payload_value_str) != 0) {
+                            match = 0; // Set match flag to false if values are not equal
+                            break; // Break loop if a mismatch is found
+                        }
+                    } else {
+                        match = 0; // Set match flag to false if key not found in user
+                        break; // Break loop if key not found in user
+                    }
+                }
+                if (match) {
+                    // Add user to the JSON array
+                    json_object_array_add(user_array, user);
+                } else {
+                    json_object_put(user); // Free JSON object if not added to array
+                }
+            }
+        }
+
+        fclose(fp);
+    }
+
+    closedir(dir);
+
+    const char *json_str = json_object_to_json_string(user_array); // Convert JSON array to string
+
+    size_t len = strlen(json_str);
+    char *resp = (char *)malloc(len + 1);
+    strncpy(resp, json_str, len);
+    resp[len] = '\0';
+
+    json_object_put(user_array); // Release JSON array memory
+
+    return resp;
+}
+
+const char *listar_usuario(const char *email, const char *caminho) {
+    char caminho_completo[100];
+    sprintf(caminho_completo, "%s/%s.json", caminho, email); // Create complete file path for the given email
+
+    FILE *fp = fopen(caminho_completo, "r");
+    if (fp == NULL) {
+        printf("Não foi possível abrir o arquivo %s\n", caminho_completo);
+        return NULL;
+    }
+
+    char buffer[4096];
+    json_object *user = NULL;
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        user = json_tokener_parse(buffer); // Parse JSON from file
+        if (user != NULL) {
+            break; // Break loop if user is found
+        }
+    }
+
+    fclose(fp);
+
+    if (user == NULL) {
+        printf("Usuário com email %s não encontrado\n", email);
+        return NULL;
+    }
+
+    const char *json_str = json_object_to_json_string(user); // Convert JSON object to string
+
+    size_t len = strlen(json_str);
+    char *resp = (char *)malloc(len + 1);
+    strncpy(resp, json_str, len);
+    resp[len] = '\0';
+
+    json_object_put(user); // Release JSON object memory
+
+    return resp;
 }
 
 // Cria um arquivo e armazena o json de entrada nele
-void armazenar_info_usuario(json_object *json_obj, const char *email, const char *caminho) {
+void armazenar_info_usuario(json_object *json_obj, const char *caminho) {
+    // Get email from json object
+    json_object *email_obj = NULL;
+    if (json_object_object_get_ex(json_obj, "email", &email_obj) == 0) {
+        printf("Campo 'email' não encontrado no objeto JSON\n");
+        return;
+    }
+
+    const char *email = json_object_get_string(email_obj);
+
     // Criar arquivo com o email como nome do arquivo
     char nome_arquivo[100];
     sprintf(nome_arquivo, "%s/%s.json", caminho, email);
@@ -105,51 +268,62 @@ void *lidar_com_cliente(void *arg) {
 
         }
 
-        // Analisar objeto JSON
+        // Parse JSON from received buffer
         json_object *json_obj = json_tokener_parse(buffer);
+
         if (json_obj == NULL) {
-            printf("Objeto JSON inválido.\n");
+            printf("Erro ao analisar o JSON\n");
             continue;
         }
 
-        // Extrair campos do Json
-        json_object *nome_obj, *email_obj, *sobrenome_obj, *cidade_obj, *formacao_obj, *ano_obj, *habilidades_obj;
-        if (json_object_object_get_ex(json_obj, "nome", &nome_obj) &&
-            json_object_object_get_ex(json_obj, "sobrenome", &sobrenome_obj) &&
-            json_object_object_get_ex(json_obj, "cidade", &cidade_obj) &&
-            json_object_object_get_ex(json_obj, "formacao", &formacao_obj) &&
-            json_object_object_get_ex(json_obj, "ano", &ano_obj) &&
-            json_object_object_get_ex(json_obj, "email", &email_obj) &&
-            json_object_object_get_ex(json_obj, "habilidades", &habilidades_obj)) {
-            const char *nome = json_object_get_string(nome_obj);
-            const char *email = json_object_get_string(email_obj);
-            const char *sobrenome = json_object_get_string(sobrenome_obj);
-            const char *cidade = json_object_get_string(cidade_obj);
-            const char *formacao = json_object_get_string(formacao_obj);
-            const char *ano = json_object_get_string(ano_obj);
-            const char *habilidades = json_object_get_string(habilidades_obj);
+        // Extract "command" and "payload" from JSON
+        json_object *command_obj, *payload_obj;
+        if (json_object_object_get_ex(json_obj, "command", &command_obj) &&
+            json_object_object_get_ex(json_obj, "payload", &payload_obj)) {
+            const char *command = json_object_get_string(command_obj);
+            json_object *payload = payload_obj;
 
-            printf("Objeto JSON recebido:\n");
-            printf("Nome: %s\n", nome);
-            printf("Sobrenome: %s\n", sobrenome);
-            printf("Email: %s\n", email);
-            printf("Ano: %s\n", ano);
-            printf("Cidade: %s\n", cidade);
-            printf("Formação: %s\n", formacao);
-            printf("Habilidades: %s\n", habilidades);
-
-            armazenar_info_usuario(json_obj, email, DB_USERS_PATH); //colocar aqui o path pra pasta de users
-
-            printf("Chamando funcao pra printar todos os usuarios registrados:\n");
+            if (strcmp(command, "new_user") == 0) {
+                // Create new user
+                armazenar_info_usuario(payload, DB_USERS_PATH);
+                // Send response to client
+                const char *response = "Novo usuário criado com sucesso!\n";
+                send(novo_sockfd, response, strlen(response), 0);
+            } else if (strcmp(command, "list_all_users") == 0) {
+                // List all users
+                const char *response = listar_todos_usuarios(DB_USERS_PATH);
+                // Send response to client
+                send(novo_sockfd, response, strlen(response), 0);
+            } else if (strcmp(command, "delete_user") == 0) {
+                // Delete user
+                const char *email = json_object_get_string(payload);
+                deletar_usuario(email, DB_USERS_PATH);
+                // Send response to client
+                const char *response = "Usuário deletado com sucesso!\n";
+                send(novo_sockfd, response, strlen(response), 0);
+            } else if (strcmp(command, "query_users") == 0) {
+                // Query users
+                const char *response = query_usuarios(payload, DB_USERS_PATH);
+                send(novo_sockfd, response, strlen(response), 0);
+            } else if (strcmp(command, "list_user") == 0) {
+                // List user
+                const char *email = json_object_get_string(payload);
+                const char *response = listar_usuario(email, DB_USERS_PATH);
+                if (response == NULL) response = "Não achamos ninguém com esse email\n";
+                send(novo_sockfd, response, strlen(response), 0);
+            } else {
+                // Invalid command
+                const char *response = "Comando inválido\n";
+                send(novo_sockfd, response, strlen(response), 0);
+            }
 
         } else {
-            printf("Formato de objeto JSON inválido\n");
+            // Invalid JSON format
+            const char *response = "Formato de objeto JSON inválido";
+            send(novo_sockfd, response, strlen(response), 0);
         }
 
-        json_object_put(json_obj); // Liberar objeto JSON
-
-        if (n == 0)
-            break;
+        json_object_put(json_obj);
     }
 
     close(novo_sockfd);
@@ -189,6 +363,44 @@ void *aceitar_novos_clientes() {
         }
     }
     return NULL;
+}
+
+void imprimir_arquivos_na_pasta(const char* caminho) {
+    DIR *dir;
+    struct dirent *ent;
+
+    if ((dir = opendir(caminho)) == NULL) {
+        perror("Não foi possível abrir o diretório");
+        return;
+    }
+
+    while ((ent = readdir(dir)) != NULL) {
+        // Ignorar . e ..
+        if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+            continue;
+        }
+
+        char caminho_completo[100];
+        sprintf(caminho_completo, "%s/%s", caminho, ent->d_name);
+
+        FILE* fp = fopen(caminho_completo, "r");
+        if (fp == NULL) {
+            printf("Não foi possível abrir o arquivo %s\n", caminho_completo);
+            continue;
+        }
+
+        printf("Arquivo: %s\n", caminho_completo);
+
+        char buffer[4096];
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            printf("%s", buffer);
+        }
+
+        printf("\n");
+        fclose(fp);
+    }
+
+    closedir(dir);
 }
 
 int main() {
