7f1b3d440cb5ff3dad7a49cef0f9fe0aa9ec77da
Tiago Dall'Oca <tdalloca@gmail.com>
2023-04-23 21:07:02 -0300

    estrutura principal do projeto estruturada

---
+++ client/src/client.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <json-c/json.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <dirent.h>
+#include <string.h>
+
+#define PORT 5000                       // Número da porta para o servidor
+#define BUFFER_SIZE 4096                // Tamanho do buffer de recebimento
+
+int main(int argc, char** args){
+	return 0;
+}
